e32c4b3ef1fb4ec471e6d90b9d96b319a074ae44
lzd <jiusanlzd@163.com>
2021-05-23 22:02:42 +0800

    syncMsg

--- www/Mqtt.js
+++ www/Mqtt.js
@@ -1,5 +1,6 @@
 var exec = require('cordova/exec');
 
+// client_id mqtt_host mqtt_port_tcp password username
 exports.connect = function (arg0, success, error) {
     exec(success, error, 'Mqtt', 'connect', [arg0]);
 };
@@ -8,10 +9,12 @@ exports.disConnect = function(success, error) {
     exec(success, error, 'Mqtt', 'disConnect', [{}]);
 };
 
+// 返回js success 1 -> 已连接 0 -> 连接丢失
 exports.checkStatus = function (success, error) {
     exec(success, error, 'Mqtt', 'checkStatus', [{}]);
 };
 
+// 100 连接断开 101 重新连接 102 连接成功
 exports.onMessageEvent = function (eventId, params) {
     cordova.fireDocumentEvent('Mqtt.onMessageEvent', {
         eventId: eventId,
